3ad8378cf266328079c9b6379b3dfb7e79dd129e
mashood100 <mashoodhussain100@gmail.com>
2020-06-16 19:20:49 +0500

    update app.js

--- assigment1/chap1.js
+++
@@ -1,6 +0,0 @@
-alert("Error "+ "\n \n " +"Please Enter Correct Password");
-alert("Hello user happy coding"+ "\n \n " +"welcome to Js world");
-alert("welcome to js world");
-alert("Hello user happy coding"+ "\n \n " +"welcome back");
-
-confirm("Prevent this page from creating additional dialogs");
